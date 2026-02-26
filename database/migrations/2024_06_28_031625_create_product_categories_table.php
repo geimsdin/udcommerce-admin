454abc803c04ac70224c7076c32c3fb824d786bf
hidayat <muh.hidayaat24@gmail.com>
2026-02-26 16:28:29 +0800

    adjust clients and product categories

--- database/migrations/2024_06_28_031625_create_product_categories_table.php
+++ database/migrations/2024_06_28_031625_create_product_categories_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -14,7 +13,6 @@ return new class extends Migration
         Schema::create('product_categories', function (Blueprint $table) {
             $table->id();
             $table->foreignId('parent_id')->nullable()->constrained('product_categories')->nullOnDelete();
-            $table->string('title');
             $table->integer('sort_order')->default(0);
             $table->boolean('status')->default(0);
             $table->timestamps();
